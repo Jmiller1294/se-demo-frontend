d0b5cae2c3ae036a7173432d44c74508d38f9157
Justin Miller <jayman1294@gmail.com>
2022-05-16 14:46:36 -0400

    added reviews to homepage

--- src/layout/TopHeader.js
+++ src/layout/TopHeader.js
@@ -11,6 +11,7 @@ const TopHeader = (props) => {
       <Header>
         <Title>First Responder Equipment</Title>
         <SearchBar />
+        Hello
       </Header>
     </>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,7 +24,7 @@ const BannerImg = styled.img`
   height: 500px;
   flex-basis: 100%;
 `
-const Container = styled.div`
+const PictureContainer = styled.div`
   display: flex;
   height: 300px;
   width: 100%;
@@ -32,9 +32,51 @@ const Container = styled.div`
   justify-content: space-evenly;
   margin-top: 40px;
 `
+const ReviewContainer = styled.div`
+  display: flex;
+  height: 300px;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin-top: 40px;
+  padding: 0 50px;
+`
 const ProductImage = styled.img`
   height: 350px;
   width: 20%;
+`
+const ReviewHeader = styled.h4`
+  position: relative;
+  right: 480px;
+  width: 100%;
+  font-size: 20px;
+  color: #ffffff;
+  margin-bottom: 10px;
+`
+const Line = styled.hr`
+  margin: 0 90px;
+`
+const Review = styled.p`
+  height: 400px;
+  width: 22%;
+  font-size: 18px;
+  color: #ffffff;
+  line-height: 1.5;
+`
+const InformationForm = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 400px;
+  width: 100%;
+`
+const FormInput = styled.input`
+  height: 50px;
+  width: 25%;
+`
+const FormButton = styled.button`
+  height: 50px;
+  width: 10%;
 `
 
 
@@ -54,22 +96,76 @@ const Home = () => {
         <Row  height={'500px'}>
           <Col size={1}>
            <h2>Featured Products</h2>
-           <Container>
+           <PictureContainer>
             <ProductImage src={HolsterImage}/>
             <ProductImage src={FireSuitImage}/>
             <ProductImage src={HolsterImage}/>
             <ProductImage src={VestImage}/>
-           </Container>
+           </PictureContainer>
           </Col>
         </Row>
         <Row  height={'450px'} background={'#00264D'}>
           <Col size={1}>
-           
+            <ReviewHeader>Reviews From Our Customers</ReviewHeader>
+            <Line />
+            <ReviewContainer>
+              <Review>
+                They always have the best
+                equipment at the best prices.
+                The shipping was fast and easy.
+                Helping Fire and Police Departments 
+                around the country. Thank you first Responders!
+                <br />
+                <br />
+                - Dennis Miller
+                <br />
+                5/20/20
+              </Review>
+              <Review>
+                They always have the best
+                equipment at the best prices.
+                The shipping was fast and easy.
+                Helping Fire and Police Departments 
+                around the country. Thank you first Responders!
+                <br />
+                <br />
+                - Dennis Miller
+                <br />
+                5/20/20
+              </Review>
+              <Review>
+                They always have the best
+                equipment at the best prices.
+                The shipping was fast and easy.
+                Helping Fire and Police Departments 
+                around the country. Thank you first Responders!
+                <br />
+                <br />
+                - Dennis Miller
+                <br />
+                5/20/20
+              </Review>
+              <Review>
+                They always have the best
+                equipment at the best prices.
+                The shipping was fast and easy.
+                Helping Fire and Police Departments 
+                around the country. Thank you first Responders!
+                <br />
+                <br />
+                - Dennis Miller
+                <br />
+                5/20/20
+              </Review>
+            </ReviewContainer>
           </Col>
         </Row>
         <Row  height={'400px'}>
           <Col size={1}>
-           
+            <InformationForm>
+              <FormInput type='text' />
+              <FormButton>Submit</FormButton>
+            </InformationForm>
           </Col>
         </Row>
         <Row  height={'150px'} background={'#00264D'} padding={'20px 0'}>
